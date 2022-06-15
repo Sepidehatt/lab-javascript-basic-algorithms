7b622bb9a6d8310b6c2559427c1b850e61d116e0
Sepideh Attar <sepideh.att@gmail.com>
2022-06-15 17:14:54 +0200

    done iteration 1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 // Iteration 1: Names and Input
-//
+let hacker1= "Luis"
+let hacker2= "Sepid"
+console.log(`The driver's name is ${hacker1}`)
+console.log(`The navigator's name is ${hacker2}` )
 // Iteration 2: Conditionals
 
 
 // Iteration 3: Loops
+
+let myLorem=`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vestibulum faucibus sapien, ac gravida ex mattis quis. Aliquam erat volutpat. Quisque sagittis tortor quis risus vehicula, quis commodo sapien rutrum. Mauris non lorem urna. Aenean consectetur leo pretium est sollicitudin, id tempor sem eleifend. Nunc sollicitudin lacus et maximus malesuada. Suspendisse at tortor eget mauris dignissim pulvinar. Quisque accumsan scelerisque mi id consequat. Nunc dui tellus, vehicula a malesuada viverra, rhoncus eget nisl. Sed id enim vitae nulla blandit ornare eget sit amet enim. Sed eu tincidunt ex, in facilisis nisi. Etiam nec euismod quam, vehicula eleifend mauris. Mauris turpis urna, suscipit id interdum molestie, varius at nisi. Nunc auctor mauris sed mi fermentum congue. Mauris tristique nibh vitae nulla pharetra accumsan.
+
+Vestibulum eget commodo nisi. Aliquam eleifend diam quis ante ullamcorper tincidunt. Quisque sit amet ultricies sapien. Cras gravida nisi libero, vitae vehicula sem venenatis in. Suspendisse non nunc molestie, mollis nisi et, porttitor mauris. Duis turpis tortor, tincidunt at tortor in, consectetur tempor diam. Nam placerat purus in fringilla malesuada. Cras nec fringilla nisl. Nulla imperdiet nibh sit amet risus bibendum, nec sagittis nisi blandit. Et
+iam eget felis suscipit, ullamcorper purus in, convallis turpis. Nullam nec mollis justo, convallis pharetra turpis. Nunc sed scelerisque mi. In pretium nunc sem, viverra pulvinar arcu sodales ac.
+
+Aenean vestibulum leo velit, nec sodales mi feugiat a. Ut at lobortis sem. Nullam nec elit at velit ultricies pretium. Praesent ut erat nibh. Donec vestibulum odio et porta imperdiet. Quisque eu cursus risus. Nullam quis vehicula est. Nam eros nibh, accumsan non iaculis vel, finibus vel metus. Praesent tristique nibh at dui tempor interdum. Aenean rutrum efficitur bibendum. Nulla varius neque nisi, vitae consequat ligula congue a.`
